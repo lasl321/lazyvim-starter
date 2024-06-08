138c57c586ebd0d2ba63175940ef5634d6463f75
Luis Sotomayor <luis@zuub.com>
2024-06-08 14:14:49 -0700

    feat(hardtime): change event trigger to prevent conflict with LazyVim

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,5 +1,11 @@
 return {
   "m4xshen/hardtime.nvim",
+
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+
+  -- Need event change to prevent LazyVim from taking over `gj` or `gk` key bindings
+  -- https://github.com/m4xshen/hardtime.nvim/issues/5
+  event = "VeryLazy",
+
   opts = {},
 }
