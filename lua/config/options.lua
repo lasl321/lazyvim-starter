941c3c8e49e2a80e20bc920a9d9f50fc354d5b43
Luis Sotomayor <luis@zuub.com>
2025-06-29 22:27:53 -0700

    feat: add plugin options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Write all buffers before navigating from Vim to tmux pane
+vim.g.tmux_navigator_save_on_switch = 2
+
+-- Disable tmux navigator when zooming the Vim pane
+vim.g.tmux_navigator_disable_when_zoomed = 1
